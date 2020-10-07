433b4328348bfde598b6d17800eee122e484cf46
Grisha2004 <72410090+Grisha2004@users.noreply.github.com>
2020-10-07 18:46:22 +0300

    ege24

--- Project1/Project1/ege24.cpp
+++ Project1/Project1/ege24.cpp
@@ -1,14 +1,32 @@
 #include "ege24.h"
 #include <string>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 void ege24::task27421()
 {
-	string s = "xyxyxyxyxxxzzzzxyxyz";
-
-	s[0] = 'a';
-
-	cout << s.length();
+	char n;
+	char m = 0; 
+	int max = 0;
+	int length = 0;
+	ifstream fin;
+	string path = "24_demo.txt";
+	fin.open(path);
+	while (fin.get(n)) {
+		if (n!= m) 
+			length += 1; 		
+		else {
+			length -= 1;
+			if (length > max) 
+				max = length;
+			length = 1;
+			
+			
+		}
+		m = n;
+	}
+	fin.close();
+	cout << max;
 }
